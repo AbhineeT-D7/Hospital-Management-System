6956073ea7f419cba906633d476ddf3d5698241b
AbhineeT-D7 <abhinetakg78965@gmail.com>
2026-06-05 18:48:17 +0530

    Update project files

--- src/main/java/com/abhi/gov_hos_app/security/JWTAuthorizationFilter.java
+++ src/main/java/com/abhi/gov_hos_app/security/JWTAuthorizationFilter.java
@@ -1,29 +1,41 @@
 package com.abhi.gov_hos_app.security;
 
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import java.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-public class JWTAuthorizationFilter /* extends BasicAuthenticationFilter */{
+public class JWTAuthorizationFilter  extends BasicAuthenticationFilter {
 
-//    private JwtTokenProvider jwtTokenProvider;
-//
-//    public JWTAuthorizationFilter(AuthenticationManager authenticationManager,
-//                                  JwtTokenProvider tokenProvider) {
-//        super(authenticationManager);
-//        jwtTokenProvider = tokenProvider;
-//    }
-//
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request,
-//                                    HttpServletResponse response, FilterChain chain)
-//            throws IOException, ServletException {
-//        Authentication authentication = jwtTokenProvider.getAuthentication(request);
-//
-//        if(authentication !=null && jwtTokenProvider.validateToken(request)){
-//            SecurityContextHolder.getContext().setAuthentication(authentication);
-//        }
-//        chain.doFilter(request, response);
-//    }
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager,
+                                  JwtTokenProvider tokenProvider) {
+        super(authenticationManager);
+        jwtTokenProvider = tokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain chain)
+            throws IOException, ServletException {
+        Authentication authentication = jwtTokenProvider.getAuthentication(request);
+
+        if(authentication !=null && jwtTokenProvider.validateToken(request)){
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        try {
+            chain.doFilter(request, response);
+        } catch (java.io.IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- src/main/java/com/abhi/gov_hos_app/security/JwtTokenProvider.java
+++ src/main/java/com/abhi/gov_hos_app/security/JwtTokenProvider.java
@@ -1,65 +1,96 @@
 package com.abhi.gov_hos_app.security;
 
 
-//@Component
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class JwtTokenProvider {
 //
-//    @Value("${app.jwt.secret}")
-//    private String jwtSecret;
-//
-//    @Value("${app.jwt.token.prefix}")
-//    private String jwtTokenPrefix;
-//
-//    @Value("${app.jwt.header.string}")
-//    private String jwtHeaderString;
-//
-//    @Value("${app.jwt.expiration-in-ms}")
-//    private Long jwtExpirationInMs;
-//
-//    public String generateToken(Authentication auth){
-//        String authorities = auth.getAuthorities().stream()
-//                .map(GrantedAuthority::getAuthority)
-//                .collect(Collectors.joining());
-//
-//        return Jwts.builder().setSubject(auth.getName())
-//                .claim("roles", authorities)
-//                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
-//                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
-//    }
-//
-//    public Authentication getAuthentication(HttpServletRequest request){
-//        String token = resolveToken(request);
-//        if(token == null){
-//            return null;
-//        }
-//        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
-//        String username = claims.getSubject();
-//        final List<GrantedAuthority> authorities = Arrays.stream(claims.get("roles").toString().split(","))
-//                .map(role -> role.startsWith("ROLE_")?role:"ROLE_"+role)
-//                .map(SimpleGrantedAuthority::new)
-//                .collect(Collectors.toList());
-//        return username!= null ? new UsernamePasswordAuthenticationToken(username, null, authorities): null;
-//    }
-//
-//    public boolean validateToken(HttpServletRequest request){
-//        String token = resolveToken(request);
-//        if(token == null){
-//            return false;
-//        }
-//        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
-//        if(claims.getExpiration().before(new Date())){
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    private String resolveToken(HttpServletRequest req){
-//        //Bearer key...
-//        String bearerToken = req.getHeader(jwtHeaderString);
-//        if(bearerToken!=null && bearerToken.startsWith(jwtTokenPrefix)){
-//            return bearerToken.substring(7, bearerToken.length());
-//        }
-//        return null;
-//    }
+    @Value("${app.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${app.jwt.token.prefix}")
+    private String jwtTokenPrefix;
+
+    @Value("${app.jwt.header.string}")
+    private String jwtHeaderString;
+
+    @Value("${app.jwt.expiration-in-ms}")
+    private Long jwtExpirationInMs;
+
+    public String generateToken(Authentication auth){
+        String authorities = auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining());
+
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .subject(auth.getName())
+                .claim("roles", authorities)
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationInMs)) // Replaces setExpiration
+                .signWith(key)
+                .compact();
+    }
+
+    public Authentication getAuthentication(HttpServletRequest request){
+        String token = resolveToken(request);
+        if(token == null){
+            return null;
+        }
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        String username = claims.getSubject();
+        final List<GrantedAuthority> authorities = Arrays.stream(claims.get("roles").toString().split(","))
+                .map(role -> role.startsWith("ROLE_")?role:"ROLE_"+role)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return username!= null ? new UsernamePasswordAuthenticationToken(username, null, authorities): null;
+    }
+
+    public boolean validateToken(HttpServletRequest request){
+        String token = resolveToken(request);
+        if(token == null){
+            return false;
+        }
+        SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return !claims.getExpiration().before(new Date());
+    }
+
+    private String resolveToken(HttpServletRequest req){
+        //Bearer key...
+        String bearerToken = req.getHeader(jwtHeaderString);
+        if(bearerToken!=null && bearerToken.startsWith(jwtTokenPrefix)){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 
 }
